ec5d976d575f6e10d6a3135a2457f1d39e9b6e06
Hamiz Muzaffer <hamiz.muzaffer77@gmail.com>
2023-09-15 22:56:58 +0500

    Tasks 12-16

---
+++ husban.js
@@ -0,0 +1,102 @@
+// chapter 12-13
+//Q1
+//   alpahbet= prompt("enter alphabet")
+
+// {
+//     if (result>=65 && result<=90){
+//     alert("Uppercase")
+// }
+// else(result>=48 &&result<=57)
+//     alert("Lowercase")
+// }
+
+//Q2
+
+
+
+// var int1= prompt("Enter Number1")
+// var int2= prompt("Enter Number2")
+
+//     if(int1<int2){
+//         alert(int1, "is greater")
+//     }
+//     else if (int1=int2){
+//        alert("equal")
+//     }
+//     else{
+//     alert(int2,"\n is greater")}
+
+//Q3
+
+// var number = prompt("enter number")
+// {
+//     if(number<0){
+//         alert("Negative")
+//     }
+//     else if (number == 0){
+//         alert ("zero")
+//     }
+//     else{
+//         alert("Positive")
+//     }
+// }
+
+// Q4
+// var string = "s"
+
+//     if((string=="a")|| (string=="e")||(string=="i")||(string=="o")||(string=="u") ){
+// alert("Vowel")
+// }
+// else{
+//     alert("not vowel")}
+// }
+ 
+
+// Q5
+
+// var pass1= prompt("Enter Password")
+// var pass2 = prompt("Enter new password")
+// if(pass1==pass2)
+
+//     alert("Correct")
+
+// else 
+//     alert("incorrect")
+
+// Q6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+// CHAPTER 14-16
+//Q1
+var arr = [ ];
+// Q2
+var arr2 = [];
+// Q3
+var arr3 = ["hamiz", "najma", "husban", ]
+// q4
+ 
+var arr4 = [1,2,2,35,67,34]
+// Q5
+var arr5 = [true, false ]
+// Q6
+var arr6 = [2,4,"Husban", true, 77]
+// Q7
+var qual = ["SSC", "HSC", "BCS", 
+    "BS", "BCOM", "MS", "M. Phil." , "PhD"]
+    document.write(qual)
+// Q8
+var name1 = ['Michael',    
+'Abdullah', 'Ashok' ]   
+var marks = [23,48,55]
+for(i=0;i<=2;++i){
+    document.write("\n", name1[i],"got", marks[i], "marks \n")
+    }
+    
+// Q9
+
